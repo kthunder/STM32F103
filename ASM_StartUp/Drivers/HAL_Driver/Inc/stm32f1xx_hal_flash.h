b13ce893a68341294c71e74faaca3ae33d864401
konglei <konglei@thinktech.net.cn>
2023-06-12 12:27:27 +0800

    add flash driver

---
+++ ASM_StartUp/Drivers/HAL_Driver/Inc/stm32f1xx_hal_flash.h
@@ -0,0 +1,10 @@
+#ifndef __STM32F1xx_HAL_FLASH_H
+#define __STM32F1xx_HAL_FLASH_H
+
+#include "stm32f1xx.h"
+
+// void FLASH_Init(FLASH_TypeDef* FLASHx);
+
+// uint32_t FLASH_Transmit(FLASH_TypeDef* FLASHx, uint8_t *pData, uint16_t nSize);
+
+#endif
---
+++ ASM_StartUp/Drivers/HAL_Driver/Src/stm32f1xx_hal_flash.c
@@ -0,0 +1,3 @@
+#include "stm32f1xx_hal_flash.h"
+#include "stm32f103x6.h"
+#include "stm32f1xx.h"

ded0118e6821175fe9260ecb24fbe8a0dae486cd
konglei <konglei@thinktech.net.cn>
2023-06-21 13:39:19 +0800

    add

--- src/Core/Src/stm32f1xx_irq.c
+++ src/Core/Src/stm32f1xx_irq.c
@@ -1,4 +1,8 @@
+#include "tools.h"
+#include "stm32f1xx.h"
+#include <stdint.h>
 #include <stdio.h>
+#include "log.h"
 
 // STM IRQHandler def
 
@@ -10,3 +14,30 @@ void HardFault_Handler()
         ;
     }
 }
+
+uint32_t SVC_Handler_Fn(uint32_t * pwdSF)
+{
+    uint32_t SVC_number;
+    uint32_t SVC_r0;
+    uint32_t SVC_r1;
+    uint32_t SVC_r2;
+    uint32_t SVC_r3;
+    uint32_t nRet;
+
+    SVC_number = ((char *) pwdSF[6])[-2];
+}
+
+
+void SVC_Handler()
+{
+    __ASM volatile(         \
+    "                       \
+    IMPORT SVC_Handler_Fn   \
+    TST     LR,#4           \
+    ITE     EQ              \
+    MRSEQ   R0, MSP         \
+    MRSEQ   R0, MSP         \
+    B       svc_handler     \
+    ");
+}
+
